ac3afe7cbda59564a12e1e5df927afb546ef7767
Shawn11564 <shawnmicheldev@gmail.com>
2023-11-01 01:37:26 -0400

    Initial commit

---
+++ src/main/java/dev/mrshawn/minehunter/MineHunterModClient.java
@@ -0,0 +1,12 @@
+package dev.mrshawn.minehunter;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MineHunterModClient implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+
+	}
+
+}
